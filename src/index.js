7bd7e530d132fca7841d5fef1184659e74ab6cbc
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-04-21 09:05:15 +0900

    stateでのルーティング(最適でない)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { useState } from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Home = () => {
+  return (
+    <div> <h2>ryota notes app</h2> </div>
+  )
+}
+
+// 1行なら return なしでいきなり()でかける
+const Notes = () => (
+  <div> <h2>Notes</h2> </div>
+)
+
+const Users = () => (
+  <div> <h2>Users</h2> </div>
+)
+
+const App = () => {
+  const [page, setPage] = useState('home')
+
+  const toPage = (page) => (event) => {
+    event.preventDefault()
+    setPage(page)
+  }
+
+  const content = () => {
+    if (page === 'home') {
+      return <Home />
+    } else if (page === 'notes') {
+      return <Notes />
+    } else if (page === 'users') {
+      return <Users />
+    }
+  }
+
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <div>
+      <div>
+        <a href="" onClick={toPage('home')} style={padding}>
+          home
+        </a>
+        <a href="" onClick={toPage('notes')} style={padding}>
+          notes
+        </a>
+        <a href="" onClick={toPage('users')} style={padding}>
+          users
+        </a>
+      </div>
+
+      {content()}
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
